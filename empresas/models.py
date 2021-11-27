d910faea6e7cf39424cc1c6b571bfa5b08656e2d
WillamesCampos <willwjccampos@gmail.com>
2021-11-27 16:24:31 -0300

    criando app empresas

---
+++ empresas/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from uuid import uuid4
+
+class Empresa(models.Model):
+    codigo = models.UUIDField(
+        primary_key=True,
+        editable=False,
+        default=uuid4
+    )
+    nome = models.CharField(
+        max_length=100,
+        help_text='Nome da empresa'
+    )
+
+    class Meta:
+        def __str__(self) -> str:
+            return self.nome
