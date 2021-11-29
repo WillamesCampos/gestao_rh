fab6f22cd10fe000cedccabe4e5d86eea4c732cf
WillamesCampos <willwjccampos@gmail.com>
2021-11-29 18:52:25 -0300

    criacao app departamentos

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'empresas',
-    'funcionarios'
+    'funcionarios',
+    'departamentos'
 ]
 
 MIDDLEWARE = [
