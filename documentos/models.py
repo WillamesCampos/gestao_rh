59a8e1cc059b888547e6e1338e916ba949a2a75c
WillamesCampos <willwjccampos@gmail.com>
2021-11-30 01:14:39 -0300

    criando relacionamentos no banco de dados

--- documentos/models.py
+++ documentos/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    dono = models.ForeignKey(
+        Funcionario, on_delete=models.PROTECT
+    )
 
     def __str__(self) -> str:
         return self.descricao
--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from departamentos.models import Departamento
+from empresas.models import Empresa
+
+from uuid import uuid4
 
 class Funcionario(models.Model):
-    nome=models.CharField(
+    nome = models.CharField(
         max_length=100
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.PROTECT
+    )
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(
+        Empresa,
+        on_delete=models.PROTECT
     )
 
 
